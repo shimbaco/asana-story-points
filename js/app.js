3a2612f1ab5b8c250fa88a4a28479031568d9748
Koji Shimba <me@shimba.co>
2019-04-03 19:54:30 +0900

    利用していない処理を削除

--- js/app.js
+++ js/app.js
@@ -5,45 +5,6 @@ const MutationObserverInitOptions = {
   subtree: true,
 }
 
-function calcChecklistPoints($checkItems) {
-  var checkItemPoints = _.map($checkItems, function(checkItem) {
-    var checkItemText = $(checkItem).text()
-    var match = checkItemText.match(/\[(\d+\.*\d*)\]/)
-
-    return match ? Number(match[1]) : 0
-  })
-
-  var sum = _.reduce(
-    checkItemPoints,
-    function(sum, point) {
-      return sum + point
-    },
-    0,
-  )
-
-  return sum
-}
-
-function updateChecklistPoints() {
-  var $checkItemsLists = $('.js-checklist-items-list')
-
-  $checkItemsLists.each(function() {
-    var $checkItems = $(this).find('.js-checkitem-name')
-    var point = Math.round(calcChecklistPoints($checkItems) * 100) / 100
-    var $title = $(this)
-      .parents('.checklist')
-      .find('h3')
-
-    if ($title.find('span').length) {
-      $pointElm = $title.find('span')
-    } else {
-      $pointElm = $('<span>').appendTo($title)
-    }
-
-    $pointElm.text(' (' + point + ')')
-  })
-}
-
 const _getPoint = ($cardTitles, pointRegex) => {
   const points = $cardTitles.map(i => {
     const titleText = $($cardTitles[i]).text()
@@ -61,7 +22,6 @@ const _getPoint = ($cardTitles, pointRegex) => {
 }
 
 const _updateListPoints = ($headerTitle, pointType, point) => {
-  // console.log('$cardTitles: ', $cardTitles)
   const pointConfigData = {
     estimate: {
       pointClassName: 'js-estimate-point',
@@ -73,8 +33,6 @@ const _updateListPoints = ($headerTitle, pointType, point) => {
     },
   }
   const { pointClassName, pointTemplate } = pointConfigData[pointType]
-
-  // console.log('estimatePoint: ', estimatePoint)
 
   let $point
   if ($headerTitle.find(`span.${pointClassName}`).length) {
@@ -91,7 +49,6 @@ const _updateListPoints = ($headerTitle, pointType, point) => {
 const updateListPoints = () => {
   // ボード内の各リストをグルグルする
   $('.SortableItem').each((_i, sortableItem) => {
-    // console.log('sortableItem: ', sortableItem)
     const $cardTitles = $(sortableItem).find('.BoardCardWithCustomProperties-name')
     const $headerTitle = $(sortableItem).find('.BoardColumnHeaderTitle')
 
@@ -107,7 +64,6 @@ const mutationObserver = new MutationObserver(
   _.debounce(function(mutations) {
     mutations.forEach(mutation => {
       const $target = $(mutation.target)
-      console.log('$target: ', $target)
 
       if (
         // モーダルでタスクのタイトルを編集したとき
@@ -117,31 +73,9 @@ const mutationObserver = new MutationObserver(
       ) {
         updateListPoints()
       }
-
-      if (
-        // モーダルでタスクのタイトルを編集したとき
-        $target.hasClass('simpleTextarea--dynamic', 'simpleTextarea', 'autogrowTextarea-input') ||
-        // チェックリストが更新されたとき
-        $target.hasClass('js-checkitem-name') ||
-        // チェックリストにタスクが追加されたとき
-        $target.hasClass('js-show-checked-items') ||
-        // チェックリストのタイトルを編集したとき
-        $target.hasClass('hide-on-edit') ||
-        // 一覧ページからカードを選択したとき
-        $target.hasClass('js-list-actions') ||
-        $target.hasClass('js-list-header')
-      ) {
-        updateChecklistPoints()
-      }
     })
   }),
   500,
 )
-
-// $(function() {
-//   setTimeout(function() {
-//     updateChecklistPoints()
-//   }, 3000)
-// })
 
 mutationObserver.observe(document.body, MutationObserverInitOptions)
